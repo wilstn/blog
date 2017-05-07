d1d63452f37e9f19b5295281a5988dc82f9635af
icebear <wilsontan.2088@gmail.com>
2017-05-07 17:51:51 +0800

    Updated seed data to use new users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 require 'ffaker'
 
-20.times do
-  Blog.create(
+seed_user = User.find_by(username: "John") || User.create( username: "John", password: "123", password_confirmation: "123", email: "xxx@xxx.com")
+
+10.times do
+  seed_user.blogs.create(
                 title: FFaker::Movie.title ,
-                author: FFaker::Name.name,
                 category: FFaker::Book.genre,
                 post: FFaker::Book.description
               )
+end
+
+seed_blog = seed_user.blogs.last
+2.times do
+  sc = seed_blog.comments.new(remark: FFaker::Tweet.tweet)
+  sc.user = seed_user
+  sc.save
 end
